d741d9c2db4db14b6110b918b34cd2a2eed4d9d5
Keegan Green <keegan.green@bluwave-ai.com>
2025-08-16 11:58:18 -0400

    refactor: format all code using prettier

--- tests/panel.spec.ts
+++ tests/panel.spec.ts
@@ -4,7 +4,9 @@ test('should render nested evaluated expressions', async ({
   gotoPanelEditPage,
   readProvisionedDashboard,
 }) => {
-  const dashboard = await readProvisionedDashboard({ fileName: 'dashboard.json' });
+  const dashboard = await readProvisionedDashboard({
+    fileName: 'dashboard.json',
+  });
   const panelEditPage = await gotoPanelEditPage({ dashboard, id: '1' });
   await expect(panelEditPage.panel.locator).toContainText('x := true');
 });
@@ -12,22 +14,28 @@ test('should render nested evaluated expressions', async ({
 test('should enable expanding/collapsing of evaluated expressions via buttons', async ({
   gotoPanelEditPage,
   readProvisionedDashboard,
-  page
+  page,
 }) => {
-  const dashboard = await readProvisionedDashboard({ fileName: 'dashboard.json' });
+  const dashboard = await readProvisionedDashboard({
+    fileName: 'dashboard.json',
+  });
   const panelEditPage = await gotoPanelEditPage({ dashboard, id: '1' });
-  panelEditPage.panel.locator.getByTestId('51909b4d-7469-4244-bd09-10377e243495 button').click()
-  await expect(panelEditPage.panel.locator).toContainText('because')
-  await expect(panelEditPage.panel.locator).toContainText('a := true')
-  await expect(panelEditPage.panel.locator).toContainText('and')
-  await expect(panelEditPage.panel.locator).toContainText('b := true')
+  panelEditPage.panel.locator
+    .getByTestId('51909b4d-7469-4244-bd09-10377e243495 button')
+    .click();
+  await expect(panelEditPage.panel.locator).toContainText('because');
+  await expect(panelEditPage.panel.locator).toContainText('a := true');
+  await expect(panelEditPage.panel.locator).toContainText('and');
+  await expect(panelEditPage.panel.locator).toContainText('b := true');
 });
 
 test('should display "No data" in case panel data is empty', async ({
   gotoPanelEditPage,
   readProvisionedDashboard,
 }) => {
-  const dashboard = await readProvisionedDashboard({ fileName: 'dashboard.json' });
+  const dashboard = await readProvisionedDashboard({
+    fileName: 'dashboard.json',
+  });
   const panelEditPage = await gotoPanelEditPage({ dashboard, id: '2' });
   await expect(panelEditPage.panel.locator).toContainText('No data');
 });
@@ -36,7 +44,11 @@ test('should list any fields missing in panel data', async ({
   gotoPanelEditPage,
   readProvisionedDashboard,
 }) => {
-  const dashboard = await readProvisionedDashboard({ fileName: 'dashboard.json' });
+  const dashboard = await readProvisionedDashboard({
+    fileName: 'dashboard.json',
+  });
   const panelEditPage = await gotoPanelEditPage({ dashboard, id: '3' });
-  await expect(panelEditPage.panel.locator).toContainText('Missing required field(s): id, parent_id, name, value, operator');
+  await expect(panelEditPage.panel.locator).toContainText(
+    'Missing required field(s): id, parent_id, name, value, operator'
+  );
 });
